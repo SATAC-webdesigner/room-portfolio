1bc5d29c2837c5a8381d4294fd1b1d3ecae16636
SATAC <satac.webdesigner@gmail.com>
2023-05-27 17:32:19 +0200

    v1

--- Experience/World/Room.js
+++ Experience/World/Room.js
@@ -13,6 +13,19 @@ export default class Room {
     }
 
     setModel() {
+        this.actualRoom.children.forEach((child) => {
+            child.castShadow = true;
+            child.recieveShadow = true;
+
+            console.log(child);
+
+            if(child instanceof THREE.Group) {
+                child.children.forEach((groupchild) => {
+                    groupchild.castShadow = true;
+                    groupchild.recieveShadow = true;
+                })
+            }
+        })
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.11,0.11,0.11);
         this.actualRoom.rotation.y = Math.PI;
