fe38e29a5f9c903cebecff3347ba66666466f801
SATAC <satac.webdesigner@gmail.com>
2023-06-06 21:57:14 +0200

    v1

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -46,17 +46,17 @@ export default class Camera {
 
         this.scene.add(this.orthographicCamera);
 
-        this.helper = new THREE.CameraHelper(this.orthographicCamera);
-        this.scene.add(this.helper);
+        // this.helper = new THREE.CameraHelper(this.orthographicCamera);
+        // this.scene.add(this.helper);
 
         const size = 20;
-        const divisions = 20;
+        // const divisions = 20;
 
-        const gridHelper = new THREE.GridHelper(size, divisions);
-        this.scene.add(gridHelper);
+        // const gridHelper = new THREE.GridHelper(size, divisions);
+        // this.scene.add(gridHelper);
 
-        const axesHelper = new THREE.AxesHelper(size);
-        this.scene.add(axesHelper);
+        // const axesHelper = new THREE.AxesHelper(size);
+        // this.scene.add(axesHelper);
     }
 
     setOrbitControls() {
@@ -80,9 +80,9 @@ export default class Camera {
 
     update() {
         this.controls.update();
-        this.helper.matrixWorldNeedsUpdate = true;
-        this.helper.update();
-        this.helper.position.copy(this.orthographicCamera.position);
-        this.helper.rotation.copy(this.orthographicCamera.rotation);
+        // this.helper.matrixWorldNeedsUpdate = true;
+        // this.helper.update();
+        // this.helper.position.copy(this.orthographicCamera.position);
+        // this.helper.rotation.copy(this.orthographicCamera.rotation);
     }
 }
